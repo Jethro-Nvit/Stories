9d60312aab1e728cff6b82b3a82f294a69938a23
Emmamike1 <onyiloyi101@gmail.com>
2024-03-11 15:39:28 +0100

    Initial commit

---
+++ first_script.sh
@@ -0,0 +1,20 @@
+/usr/bin/bash
+echo "This is my first bash script"
+echo "This intersting and promising"
+
+
+# Practice Exercise 
+# 1. Write a bash script that outputs the followinf to standard output 
+
+echo "I am learning bash script"
+echo "Bash script is beautiful for automation of some tasks"
+
+# Practice Exercise
+# Write a bash script that list five tasks that can be automated using bash script 
+#
+
+1. Troubleshooting and Audits
+2. Monitor system periodically
+3. send notifications 
+4. Enhances collaboration 
+5. Reproducibility.....
