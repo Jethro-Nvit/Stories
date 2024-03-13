5230fe1852eccec0a33ec28f81d3b94e83d5086e
Emmamike1 <onyiloyi101@gmail.com>
2024-03-13 13:36:33 +0100

    added new script

--- exercise-script.sh
+++ exercise-script.sh
@@ -1,49 +1,6 @@
 #!/bin/bash
+read -p "what is your name " name 
+read -p "what is your age "  Age 
 
-# Function to check if git is already initialized
-check_git_init() {
-    if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
-        echo "Git is already initialized."
-    else
-        git init
-        echo "Git repository initialized."
-    fi
-}
-
-# Function to set global configurations
-set_global_config() {
-    read -p "Enter your global git user.name: " name
-    read -p "Enter your global git user.email: " email
-    git config --global user.name "$name"
-    git config --global user.email "$email"
-}
-
-# Function to create a new branch and switch to it
-create_and_checkout_branch() {
-    git checkout -b main
-}
-
-# Function to add files to staging
-git_add() {
-    git add .
-}
-
-# Function to commit changes
-git_commit() {
-    read -p "Enter commit message: " commit_message
-    git commit -m "$commit_message"
-}
-
-# Function to add remote repository
-git_remote_add() {
-    read -p "Enter remote repository URL: " remote_url
-    git remote add origin "$remote_url"
-}
-
-# Function to push changes
-git_push() {
-    read -p "Enter your git username: " username
-    read -s -p "Enter your git password: " password
-    echo
-    git push -u origin main
-}
+echo "my name is: $name"
+echo "my Age is: $Age" 
---
+++ file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "This is my first bash script!!!"
+echo "This interesting and promising"
+
